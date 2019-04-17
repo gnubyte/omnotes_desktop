4f54a17c491398f30f17d0a3c1ddd192cca93b9a
PH <phastings@openmobo.com>
2019-04-17 19:55:05 -0400

    initial PoC app renderer working

---
+++ app/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({show: false});
+    mainWindow.webContents.openDevTools()
+    mainWindow.loadFile('app/index.html');
+    mainWindow.once('ready-to-show', ()=> {
+        mainWindow.show();
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
